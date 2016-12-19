214ba62c1be85dbd415220dd48616eb086151291
raul-ortega <raul-ortega@users.noreply.github.com>
2016-12-19 17:37:13 +0100

    Added easeInOutCubic

--- src/main/tracker/math.h
+++ src/main/tracker/math.h
@@ -14,5 +14,6 @@ float easeTilt(float t, float b, float c, float d);
 float easeOutQuart(float t, float b, float c, float d);
 float easeOutCirc(float t, float b, float c, float d);
 float easeOutExpo(float t, float b, float c, float d);
+float easeInOutCubic(float t, float b, float c, float d);
 float easeOutCubic(float t, float b, float c, float d);
 #endif
