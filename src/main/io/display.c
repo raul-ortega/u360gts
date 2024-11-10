482730966fe65e33070cbfef309164ca342bc730
Raúl Ortega <raul.ya@gmail.com>
2024-11-11 00:23:39 +0100

    Resolve conflict

--- src/main/io/display.c
+++ src/main/io/display.c
@@ -757,32 +757,18 @@ static void showTelemetryPage(void)
     OLED_set_line(rowIndex++);
     OLED_send_string(lineBuffer);
 
-<<<<<<< HEAD
-    if (!PROTOCOL(TP_MFD))
-    {
-        if (homeSet_BY_GPS && homeSet)
-            tfp_sprintf(lineBuffer, "HOME SET <GPS>");
-        else if (!homeSet_BY_GPS && homeSet)
-            tfp_sprintf(lineBuffer, "HOME SET <AIRCRAFT>");
-        else if (!homeSet || homeReset)
-            tfp_sprintf(lineBuffer, "HOME NOT SET");
-        padLineBuffer();
-        OLED_set_line(rowIndex++);
-        OLED_send_string(lineBuffer);
-=======
     if(!PROTOCOL(TP_MFD)) {
-    	if(homeSet_BY_GPS && homeSet)
+    	if (homeSet_BY_GPS && homeSet)
     		tfp_sprintf(lineBuffer, "HOME SET <GPS>");
-    	else if(homeSet_BY_LAST && homeSet)
+    	else if (homeSet_BY_LAST && homeSet)
     	            tfp_sprintf(lineBuffer, "HOME SET <LAST>");
-    	else if(homeSet)
+    	else if (homeSet)
     		tfp_sprintf(lineBuffer, "HOME SET <AIRCRAFT>");
-    	else if(!homeSet || homeReset)
+    	else if (!homeSet || homeReset)
     		tfp_sprintf(lineBuffer, "HOME NOT SET");
 		padLineBuffer();
 		i2c_OLED_set_line(rowIndex++);
 		i2c_OLED_send_string(lineBuffer);
->>>>>>> development
     }
 
     if (!feature(FEATURE_GPS) && !feature(FEATURE_VBAT) && !feature(FEATURE_RSSI_ADC) && !(rxConfig->rssi_channel > 0))
